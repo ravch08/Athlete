13361590fea10c429a5603523b58f2ecfc7bc67e
Ravi <chauhan.ravi100891@gmail.com>
2022-05-04 12:23:00 +0530

    use & forward + Swiper

--- js/script.js
+++ js/script.js
@@ -1,260 +1,202 @@
 'use strict';
 
 const navItem = document.querySelector(".nav-item");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-
 const popupModal = document.querySelector(".popUp");
-const overlayModal = document.querySelector(".modal-overlay");
 const closeModal = document.querySelector(".popup-close");
+const scrollToTop = document.querySelector('.scrollToTop');
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const overlayModal = document.querySelector(".modal-overlay");
 
 const sliderSection = document.getElementById('slider');
-const scrollToTop = document.querySelector('.scrollToTop');
 
 
 // -- Event Listeners  ---------------------------------------------------------------------------
 
 scrollToTop.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
 });
 
-// window.onload = () => {
-//     setTimeout(function () {
-//         overlayModal.style.display = "block";
-//         popupModal.style.display = "block";
-//     }, 2000);
-// };
+document.onload = () => {
+	setTimeout(function () {
+		overlayModal.style.display = "block";
+		popupModal.style.display = "block";
+	}, 2000);
+};
 
 closeModal.addEventListener('click', () => {
 
-    popupModal.style.display = "none";
-    overlayModal.style.display = "none";
+	popupModal.style.display = "none";
+	overlayModal.style.display = "none";
 });
-
 
 
 // -- Intersection Observer ---------------------------------------------------------------------------
 
 let scrollOptions = {
-    rootMargin: '-80% 0px 0px 0px'
+	rootMargin: '-80% 0px 0px 0px'
 };
 
 let sliderObserver = new IntersectionObserver(function (entries) {
 
-    entries.forEach(entry => {
+	entries.forEach(entry => {
 
-        if (!entry.isIntersecting) {
-            scrollToTop.style.opacity = "1";
-            scrollToTop.style.right = "3%"
-        } else {
-            scrollToTop.style.right = "-10%"
-        }
-    });
+		if (!entry.isIntersecting) {
+			scrollToTop.style.opacity = "1";
+			scrollToTop.style.right = "3%"
+		} else {
+			scrollToTop.style.right = "-10%"
+		}
+	});
 
 }, scrollOptions);
 
 sliderObserver.observe(sliderSection);
 
-
-
 // -- SWIPER JS  ---------------------------------------------------------------------------
 
-const swiper = new Swiper('.swiper', {
+const swiperSlider = new Swiper('#swiper-slider', {
 
-    loop: true,
-    // effect: "fade",
-    spaceBetween: 5,
-    // grabCursor: true,
-    // mousewheel: true,
-    // slidesPerView: 1,
-    centeredSlides: true,
-    direction: 'horizontal',
+	init: true,
+	loop: true,
+	speed: 1500,
+	effect: "fade",
+	keyboard: true,
+	spaceBetween: 5,
+	centeredSlides: true,
+	direction: 'horizontal',
 
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-        clickable: 'true'
-    },
-
-    keyboard: {
-        enabled: true,
-    }
-
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
 
+const swiperEssential = new Swiper('#swiper-essential', {
 
+	init: true,
+	loop: true,
+	speed: 1500,
+	spaceBetween: 0,
+	centeredSlides: false,
+	slidesPerView: "4",
 
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: 'true'
+	},
 
-// -- OWL CAROUSEL  ---------------------------------------------------------------------------
-
-$('#brand-carousel').owlCarousel({
-    autoplay: 800,
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: true,
-
-    responsive: {
-
-        0: {
-            items: 4
-        },
-
-        564: {
-            items: 5
-        },
-
-        768: {
-            items: 8
-        },
-
-        992: {
-            items: 12
-        }
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
 
-$('#essential-carousel').owlCarousel({
-    autoplay: 800,
-    loop: true,
-    margin: 0,
-    dots: false,
-    nav: true,
+const swiperFeatures = new Swiper('#swiper-features', {
 
-    responsive: {
+	init: true,
+	loop: false,
+	speed: 1500,
+	spaceBetween: 20,
+	centeredSlides: false,
+	slidesPerView: "4",
 
-        0: {
-            items: 1
-        },
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: 'true'
+	},
 
-        564: {
-            items: 1
-        },
-
-        768: {
-            items: 2
-        },
-
-        992: {
-            items: 3
-        },
-
-        1200: {
-            items: 4
-        }
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
 
-$('#feature-carousel').owlCarousel({
-    autoplay: 800,
-    loop: false,
-    margin: 20,
-    dots: false,
-    nav: true,
+const swiperArrivals = new Swiper('#swiper-arrivals', {
 
-    responsive: {
+	init: true,
+	loop: true,
+	speed: 1500,
+	spaceBetween: 20,
+	centeredSlides: false,
+	slidesPerView: "5",
 
-        0: {
-            items: 1
-        },
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: 'true'
+	},
 
-        564: {
-            items: 1
-        },
-
-        768: {
-            items: 2
-        },
-
-        992: {
-            items: 3
-        },
-
-        1200: {
-            items: 4
-        }
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
 
-$('#arrival-carousel').owlCarousel({
-    autoplay: 800,
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: true,
+const swiperShoulder = new Swiper('#swiper-shoulder', {
 
-    responsive: {
+	init: true,
+	loop: true,
+	speed: 1500,
+	spaceBetween: 10,
+	slidesPerView: "3",
+	centeredSlides: false,
 
-        0: {
-            items: 1
-        },
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: 'true'
+	},
 
-        564: {
-            items: 2
-        },
-
-        768: {
-            items: 3
-        },
-
-        992: {
-            items: 4
-        },
-
-        1024: {
-            items: 5
-        }
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
 
-$('#shoulderPad-carousel').owlCarousel({
-    // autoplay: 800,
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: false,
+const swiperClothing = new Swiper('#swiper-clothing', {
 
-    responsive: {
+	init: true,
+	loop: true,
+	speed: 1500,
+	spaceBetween: 10,
+	slidesPerView: "3",
+	centeredSlides: false,
 
-        0: {
-            items: 1
-        },
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: 'true'
+	},
 
-        992: {
-            items: 2
-        },
-
-        1200: {
-            items: 3
-        }
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
 
-$('#clothing-carousel').owlCarousel({
-    // autoplay: 800,
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: false,
+const swiperBrands = new Swiper('#swiper-brands', {
 
-    responsive: {
+	init: true,
+	loop: true,
+	speed: 1500,
+	grabCursor: true,
+	spaceBetween: 20,
+	slidesPerView: "12",
+	centeredSlides: false,
 
-        0: {
-            items: 1
-        },
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		clickable: 'true'
+	},
 
-        576: {
-            items: 1
-        },
-
-        992: {
-            items: 2
-        },
-
-        1200: {
-            items: 3
-        }
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	}
 });
